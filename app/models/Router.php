809f57a2f1688b9b23471ac30f960539479de595
evtyuhov_roman <uchet-40@yandex.ru>
2025-12-25 11:36:40 +0300

    Пытаемся делать маршруты 2

---
+++ app/models/Router.php
@@ -0,0 +1,51 @@
+<?
+
+class Router {
+
+    private array $routes = [];
+    private string $uri; 
+    private string $method;
+
+    function __construct() {
+        $this->uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], "/") ;
+        $this->method = strtoupper($_SERVER['REQUEST_METHOD']) ;
+    
+        
+    }
+
+    private function add($uri, $controller, $method) {
+        $this->routes[] = [
+            'uri' => $uri,
+            'method' => $method,
+            'controller' => $controller
+        ];
+    } 
+
+    function get($uri, $controller) {
+        $this->add($uri, $controller, "GET");
+    }
+
+    function post($uri, $controller) {
+        $this->add($uri, $controller, "POST");
+    }
+
+
+    function match() {
+        $isMatch = false;
+
+        foreach ($this->routes as $route) {
+            if($route['uri'] == $this->uri
+                && $route['method'] == $this->method
+                && file_exists($route['controller'])) 
+            {
+                require_once($route['controller']);
+                $isMatch = true;
+                break;
+            }
+        }
+
+        if(!$isMatch) { abort(); };
+    }
+    
+
+}
--- public/index.php
+++ public/index.php
@@ -1,23 +1,39 @@
 <?
 
+// Раньше
+// session_start();
+// require_once dirname(__DIR__)."/config/config.php";
+// require_once MODELS."/helpers.php";
+// require_once CONFIG."/routes.php";
+
+// $db_config = require_once CONFIG."/db.php";
+// require_once MODELS."/DB.php";
+// $db = DB::getInstance()->getConnection($db_config);
+
+// // require_once C_POSTS."/index.php";
+
+// $uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], "/") ;
+// //dd($uri);
+// if (isset($routes[$uri]) && file_exists($routes[$uri])) {
+//     require_once $routes[$uri];
+// }
+// else abort();
+
 session_start();
-require_once dirname(__DIR__)."/config/config.php";
-require_once MODELS."/helpers.php";
-require_once CONFIG."/routes.php";
 
-$db_config = require_once CONFIG."/db.php";
-require_once MODELS."/DB.php";
-$db = DB::getInstance()->getConnection($db_config);
+    require_once dirname(__DIR__)."/config/config.php";
+    require_once MODELS."/helpers.php";
 
 
+    $db_config = require_once CONFIG."/db.php";
+    require_once MODELS."/DB.php";
+    $db = DB::getInctance()->getConnection($db_config);
 
-// require_once C_POSTS."/index.php";
 
-$uri = trim(parse_url($_SERVER['REQUEST_URI'])['path'], "/") ;
-//dd($uri);
-if (isset($routes[$uri]) && file_exists($routes[$uri])) {
-    require_once $routes[$uri];
-}
-else abort();
+    
+    require_once MODELS."/Router.php";
+    $router = new Router();
+    require_once CONFIG."/routes.php";
 
-?>
+    $router->match();
+   
