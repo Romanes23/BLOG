533cba9a9dde9c344a0bdf436b5472eebf6be6cc
evtyuhov_roman <uchet-40@yandex.ru>
2025-12-29 18:19:18 +0300

    ajax начало.

---
+++ public/scripts/main.js
@@ -0,0 +1,7 @@
+//alert("JASDVADSVASD");
+
+up_btn.addEventListener('click', (e)=> change_rate_fetch(e));
+down_btn.addEventListener('click', (e) => change_rate_fetch(e));
+function change_rate_fetch(e){
+    console.log(e);
+}
