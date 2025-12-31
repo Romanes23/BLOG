edcdbe2bebe6177ed9b4a119b6655073d58eec1a
evtyuhov_roman <uchet-40@yandex.ru>
2025-12-31 23:28:31 +0300

    Продолж AJAX. на кнопки UP и DOWN повесили клик

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,19 @@
-//alert("JASDVADSVASD");
 
-up_btn.addEventListener('click', (e)=> change_rate_fetch(e));
+// вешаем слушателя (addEventListene) события 'click' на обе кнопки
+up_btn.addEventListener('click', (e) => change_rate_fetch(e)); //  если какое либо событие было браузер всегда это объект(e), но есть варианты
 down_btn.addEventListener('click', (e) => change_rate_fetch(e));
+// по событию 'click' собираем объект (e) и отправляем
+//console.log(e);
 function change_rate_fetch(e){
-    console.log(e);
+console.log(e);
+ e.preventDefault();
+  let post_id = +e.target.dataset.postId; //забираем id нашего элемента и приводим его к числу (+)
+   
+ let action= false;
+    if (e.target.id === "up_btn") action=1
+    else if (e.target.id === "down_btn") action = -1;
+    alert([post_id,action]);    
+ 
+ 
 }
+
--- views/posts/show.tmpl.php
+++ views/posts/show.tmpl.php
@@ -7,12 +7,16 @@
                     <h3><?= $header ?? 'TEST' ?></h3>
                     <p><?= $post['content'] ?></p>
                       <!-- ======================= AJAX ============================ -->
-                    <div id="rate-container" class="row col-3">                            
+                    <div id="rate-container" class="row col-3">    <!--Это не форма, те кнопки на JS     -->                    
                         <button id="up_btn" class="btn btn-link col" data-post-id="<?= $post['post_id'] ?>">Up</button>      
-                        <p id="rate_p" class="col" ><?= isset($post['rate']) ? $post['rate'] : "0" ?></p>
+                        <p id="rate_p" class="col" ><?= isset($post['rate']) ? $post['rate'] : "0" ?></p> 
                         <button id="down_btn" class="btn btn-link col" data-post-id="<?= $post['post_id'] ?>">Down</button>    
                      </div>
-
+<!--Кнопки id="up_btn" и id="down_btn" нужны для оценки, те установки рейтинга поста +1 -1 $post['rate'].
+ AJAX здесь используется чтобы не перезагружать страницу.
+ В HTML есть способ созд свои собств данные data-post-id те post-id запись ч/з тире. Но со стороны JS обращаться будем 
+   CamelCase те postId те также как и CSS стилями-->
+                     <!-- ======================= AJAX ============================ -->
 
                     <form action="posts/edit" method="GET">
                         <input type="hidden" name="post_id" value= "<?= $post['post_id'] ?>">  <!-- передачяа id через скрытое поле input -->
